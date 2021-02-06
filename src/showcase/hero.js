fc9337f4b618e295a0841dbfdf60d87bfd073a5f
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-02-06 08:18:17 +0400

    style hero section

--- src/showcase/hero.js
+++ src/showcase/hero.js
@@ -1,10 +1,49 @@
-import showcase_image from "../images/k1.jpg";
+import showcase_image from "../images/k5.jpg";
 
 export default (() => {
   const hero = document.createElement("div");
-  hero.classList.add('flex-1')
+  hero.classList.add("flex-1", "relative");
+
   const hero_image = document.createElement("img");
+  hero_image.classList.add("h-screen", "object-cover");
   hero_image.src = showcase_image;
+
+  const layer = document.createElement("div");
+  layer.classList.add(
+    "top-0",
+    "bottom-0",
+    "left-0",
+    "right-0",
+    "bg-black",
+    "absolute",
+    "opacity-80"
+  );
+
+  const greeting = document.createElement("div");
+  greeting.classList.add("w-5/12", "absolute", "top-40", "left-32");
+  greeting.innerHTML = `
+    <h1 class='text-4xl text-white leading-relaxed mb-3'>
+      Skinny people are <br>
+      easier to kidnap.<br>
+      Stay safe.<br>
+      Eat healthy.
+    </h1>
+    <h2 class='text-gray-300 mb-10'>
+      It's easier to be faithful to a restaurant than <br>
+      it is to a woman
+    </h2>
+    <div>
+      <button class="uppercase px-6 py-3 mr-3 outline-none rounded-full bg-yellow-600 text-blue-50 max-w-max shadow-sm hover:bg-yellow-500">
+        WHO WE ARE
+      </button>
+      <button class="uppercase px-6 py-3 outline-none rounded-full border border-yellow-600 text-yellow-600 max-w-max shadow-sm hover:border-yellow-500 hover:text-yellow-500">
+        OUR MENU
+      </button>
+    </div>
+  `;
+
+  hero.appendChild(layer);
+  hero.appendChild(greeting);
   hero.appendChild(hero_image);
-  return hero
+  return hero;
 })();

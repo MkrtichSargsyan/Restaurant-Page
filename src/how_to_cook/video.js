21bf43fe15b5919b289e8661bdb9e02b5c2a101e
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-02-10 03:36:27 +0400

    add video section

--- src/how_to_cook/video.js
+++ src/how_to_cook/video.js
@@ -1,22 +1,21 @@
-// import video_url from "../images/v.mp4";
-// // import video_url from "../images/armenian_khashlama.mp4";
+import video_url from "../../armenian_khashlama.mp4";
 
-// export default (() => {
-//   const video_section = document.createElement("div");
-//   video_section.classList.add("w-full", "h-96", "focus:outline-none");
+export default (() => {
+  const video_section = document.createElement("div");
+  video_section.classList.add("w-full", "h-96", "focus:outline-none");
 
-//   const player = document.createElement("video");
-//   player.classList.add("w-full", "h-full", "object-cover");
-//   player.autoplay = true;
-//   player.loop = true;
-//   player.muted = true;
-//   player.preload = true;
+  const player = document.createElement("video");
+  player.classList.add("w-full", "h-full", "object-cover");
+  player.autoplay = true;
+  player.loop = true;
+  player.muted = true;
+  player.preload = true;
 
-//   const source = document.createElement("source");
-//   source.setAttribute("src", video_url);
-//   source.setAttribute("type", "video/mp4");
+  const source = document.createElement("source");
+  source.setAttribute("src", video_url);
+  source.setAttribute("type", "video/mp4");
 
-//   player.appendChild(source);
-//   video_section.appendChild(player);
-//   return video_section;
-// })();
+  player.appendChild(source);
+  video_section.appendChild(player);
+  return video_section;
+})();
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import menu_section from "../menu&interior/menu_section.js";
 import footer from "../footer/footer.js";
 import interior_section from "../menu&interior/interior_section.js";
 import header from "../header.js";
-// import video from "../how_to_cook/video.js";
+import video from "../how_to_cook/video.js";
 
 
 export default (content)=>{
@@ -12,6 +12,6 @@ export default (content)=>{
   content.appendChild(showcase);
   content.appendChild(menu_section);
   content.appendChild(interior_section);
-  // content.appendChild(video);
+  content.appendChild(video);
   content.appendChild(footer);
 }
